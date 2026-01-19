8f6996b8756167b3afe0c27dc2bcc2af0052cab7
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2026-01-20 01:08:27 +0300

    Add methods to fetch and parse JSON from URL
    Implemented get_response_body and load_json methods to handle GET requests and JSON parsing.

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+         """
+        Sends a GET request to the URL passed in on initialization.
+        Returns the raw response body (string).
+        """
+         response = requests.get(self.url)
+         return response.content
+
 
     def load_json(self):
-        pass
+           """
+        Uses get_response_body to send a request,
+        then converts the response to Python data (list or dict).
+        """
+           response_body = self.get_response_body()
+           return json.loads(response_body)
